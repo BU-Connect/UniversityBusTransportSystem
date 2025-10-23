5389d9125461602c42484a92e349abf6feeb3da5
SOURAV DEBNATH <sdebnath22.cse@bu.ac.bd>
2025-10-23 23:59:05 +0600

    build footer section

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from '@/app/provider';
-import { Toaster } from 'sonner';
+import Providers from "@/app/provider";
+import { Toaster } from "sonner";
+
+import Footer from "@/components/common/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,13 +29,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
+        <Providers>
+          {/* Global toast notifications */}
+          <Toaster richColors position="top-center" />
 
-      <Providers>
-        <Toaster richColors position="top-center" />
-        {children}
-      </Providers>
+          {/* Page-specific content */}
+          <main className="flex-1 overflow-y-auto">{children}</main>
+
+          {/* Global Footer */}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
